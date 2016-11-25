4c9fd24e1c37af6ffa9e9ebbf9c85a624171fa9a
Nick Ball <nicholaspball@gmail.com>
2016-11-24 18:30:39 -0800

    update sw again...

--- src/sw.js
+++ src/sw.js
@@ -2,9 +2,8 @@ self.addEventListener('install', e => {
     e.waitUntil(
         caches.open('now-and-then').then(cache => {
             return cache.addAll([
-                './',
-                './index.html',
-                '/app.js'
+                'index.html',
+                'app.js'
             ]).then(() => self.skipWaiting());
         })
     );
