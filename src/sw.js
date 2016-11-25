466ef7cb78721c862663b9d2ef058239cf577705
Nick Ball <nicholaspball@gmail.com>
2016-11-24 18:58:04 -0800

    another try

--- src/sw.js
+++ src/sw.js
@@ -1,13 +1,13 @@
-const VERSION = 4;
+const VERSION = 5;
 
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open('now-and-then').then(cache => {
             return cache.addAll([
-                'index.html',
-                'app.js'
+                                '/index.html',
+                                '/app.js'
             ]).then(() => self.skipWaiting());
-        })
+        }).catch(console.error.bind(error))
     );
 });
 
@@ -20,6 +20,6 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request);
-        })
+        }).catch(console.error.bind(console))
     );
 });
